00d118501bd449a1c1180cb3b7d0d8ecf330a755
Marc Richter <mr@in-telegence.net>
2015-01-09 14:36:33 +0100

    Hinzufügen der Möglichkeit unter /extra ein Volume zu mounten, welchen ein Script namens "init" enthält, um dieses für eigene Anpassungen einbinden zu können.

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# If exist, execute /extra/init . Must exit and not stay in front
+if [ -e "/extra/init" ]; then
+    if [ ! -x "/extra/init" ]; then
+        chmod +x "/extra/init"
+    fi
+    /extra/init
+fi
+
+# Prepare DocumentRoot and start HTTPd
 chown www-data:www-data /app -R
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
